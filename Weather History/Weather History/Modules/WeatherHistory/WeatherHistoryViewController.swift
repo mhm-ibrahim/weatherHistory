2d470d63d3dde1acf6c66b7ad790b3a6bd6d30a3
Mohamed Hamed <mohamed.hamed.ibrahem@gmail.com>
2017-12-28 16:02:49 +0200

    Added location manager

--- Weather History/Weather History/Modules/WeatherHistory/WeatherHistoryViewController.swift
+++ Weather History/Weather History/Modules/WeatherHistory/WeatherHistoryViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherHistoryViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupLocationManager()
+    }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        if let location = locationManager.location {
+            let lat = location.coordinate.latitude
+            let long = location.coordinate.longitude
+            
+            WeatherService.current(lat: lat, long: long, units: "metric", language: "en", onSuccess: {
+                
+            }, onFailure: { error in
+                print(error.localizedDescription)
+            })
+        }
     }
 
+}
+
+extension WeatherHistoryViewController: CLLocationManagerDelegate {
+ 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Lat \(locations.first?.coordinate.latitude), Long\(locations.first?.coordinate.longitude)")
+    }
 }
