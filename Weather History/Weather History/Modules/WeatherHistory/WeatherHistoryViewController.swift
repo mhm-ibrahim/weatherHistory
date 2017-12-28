333be2aabcbe872e2cb76816607993c8c7819628
Mohamed Hamed <mohamed.hamed.ibrahem@gmail.com>
2017-12-28 13:16:18 +0200

    added entry point

---
+++ Weather History/Weather History/Modules/WeatherHistory/WeatherHistoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherHistoryViewController.swift
+//  Weather History
+//
+//  Created by Mohamed Hamed on 12/28/17.
+//  Copyright © 2017 Hamed. All rights reserved.
+//
+
+import UIKit
+
+class WeatherHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
