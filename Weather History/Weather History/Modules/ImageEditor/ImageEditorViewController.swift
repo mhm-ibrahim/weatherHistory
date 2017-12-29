59b7672585d5525970646e2d50b0bf836df8c90f
Mohamed Hamed <mohamed.hamed.ibrahem@gmail.com>
2017-12-29 15:16:37 +0200

    added Gestures to added subviews

--- Weather History/Weather History/Modules/ImageEditor/ImageEditorViewController.swift
+++ Weather History/Weather History/Modules/ImageEditor/ImageEditorViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-//TODO:
-//Add subviews for Temp , city name ad weather condition
-// Add Gestures for pinch, rotate , remove 
+public protocol ImageEditorDelegate {
+    func doneEditing(image: UIImage)
+}
+
 class ImageEditorViewController: UIViewController {
 
+    @IBOutlet weak var canvasView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var canvasImageView: UIImageView!
+    @IBOutlet weak var imageViewHeightConstraint: NSLayoutConstraint!
+    
+    var image: UIImage!
+    var imageEditorDelegate: ImageEditorDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setImageView(image: image)
+        let emojiLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        emojiLabel.textAlignment = .center
+        emojiLabel.text = "💃🏻"
+        emojiLabel.font = UIFont.systemFont(ofSize: 80)
+        addView(view: emojiLabel)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-     }
+    func setImageView(image: UIImage) {
+        imageView.image = image
+        let size = image.suitableSize(widthLimit: UIScreen.main.bounds.width)
+        imageViewHeightConstraint.constant = (size?.height)!
+    }
+}
 
+extension ImageEditorViewController {
+    
+    func addView(view: UIView) {
+        view.center = canvasImageView.center
+        self.canvasImageView.addSubview(view)
+        addGestures(view: view)
+    }
 }
--- Weather History/Weather History/Modules/ImageViewer/Editor+Gestures.swift
+++ Weather History/Weather History/Modules/ImageViewer/Editor+Gestures.swift
@@ -7,3 +7,110 @@
 //
 
 import Foundation
+import UIKit
+
+extension ImageEditorViewController : UIGestureRecognizerDelegate  {
+    
+    func addGestures(view: UIView) {
+        //Gestures
+        view.isUserInteractionEnabled = true
+        
+        let panGesture = UIPanGestureRecognizer(target: self,
+                                                action: #selector(ImageEditorViewController.panGesture))
+        panGesture.minimumNumberOfTouches = 1
+        panGesture.maximumNumberOfTouches = 1
+        panGesture.delegate = self
+        view.addGestureRecognizer(panGesture)
+        
+        let pinchGesture = UIPinchGestureRecognizer(target: self,
+                                                    action: #selector(ImageEditorViewController.pinchGesture))
+        pinchGesture.delegate = self
+        view.addGestureRecognizer(pinchGesture)
+        
+        let rotationGestureRecognizer = UIRotationGestureRecognizer(target: self,
+                                                                    action:#selector(ImageEditorViewController.rotationGesture) )
+        rotationGestureRecognizer.delegate = self
+        view.addGestureRecognizer(rotationGestureRecognizer)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ImageEditorViewController.tapGesture))
+        view.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    @objc func panGesture(_ recognizer: UIPanGestureRecognizer) {
+        if let view = recognizer.view {
+            moveView(view: view, recognizer: recognizer)
+        }
+    }
+    
+    @objc func pinchGesture(_ recognizer: UIPinchGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+        }
+        recognizer.scale = 1
+    }
+    
+    @objc func rotationGesture(_ recognizer: UIRotationGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.rotated(by: recognizer.rotation)
+            recognizer.rotation = 0
+        }
+    }
+    
+    @objc func tapGesture(_ recognizer: UITapGestureRecognizer) {
+        if let view = recognizer.view {
+            scaleEffect(view: view)
+        }
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false
+    }
+    
+    func scaleEffect(view: UIView) {
+        view.superview?.bringSubview(toFront: view)
+        
+        if #available(iOS 10.0, *) {
+            let generator = UIImpactFeedbackGenerator(style: .heavy)
+            generator.impactOccurred()
+        }
+        let previouTransform =  view.transform
+        UIView.animate(withDuration: 0.2,
+                       animations: {
+                        view.transform = view.transform.scaledBy(x: 1.2, y: 1.2)
+        },
+                       completion: { _ in
+                        UIView.animate(withDuration: 0.2) {
+                            view.transform  = previouTransform
+                        }
+        })
+    }
+    
+    func moveView(view: UIView, recognizer: UIPanGestureRecognizer)  {
+        
+        view.superview?.bringSubview(toFront: view)
+        
+        view.center = CGPoint(x: view.center.x + recognizer.translation(in: canvasImageView).x,
+                              y: view.center.y + recognizer.translation(in: canvasImageView).y)
+        
+        recognizer.setTranslation(CGPoint.zero, in: canvasImageView)
+        
+        if recognizer.state == .ended {
+            if !canvasImageView.bounds.contains(view.center) { //Snap the view back to canvasImageView
+                UIView.animate(withDuration: 0.3, animations: {
+                    view.center = self.canvasImageView.center
+                })
+                
+            }
+        }
+        
+    }
+}
